55336cb4a44c0546a91c9391bc73cfa411539fc1
mr.khoshghamat <Mr.khoshghamat@gmail.com>
2022-08-09 00:19:22 +0430

    Login and Register Implementation

---
+++ Geram.Web/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Geram.Web.Controllers
+{
+    public class AccountController : BaseController
+    {
+        #region Login
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region Register
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
--- Geram.Web/Controllers/HomeController.cs
+++ Geram.Web/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 
 namespace Geram.Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public IActionResult Index()
         {
